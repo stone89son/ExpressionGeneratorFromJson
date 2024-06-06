6c84c42d2bd5996d62e1dfb717bb5b17a1cbc66b
son <son@gionics.llc>
2024-06-06 17:45:04 +0900

    first commit

---
+++ ConsoleApp1/ObjectHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ObjectHelper
+    {
+        public static Func<T, TResult> MakeGetter<T, TResult>(string propertyName)
+        {
+            var parameter = Expression.Parameter(typeof(T), "obj");
+            var property = Expression.Property(parameter, propertyName);
+            var lambda = Expression.Lambda<Func<T, TResult>>(property, parameter);
+
+            return lambda.Compile();
+        }
+
+        public static Action<T, TValue> MakeSetter<T, TValue>(string propertyName)
+        {
+            var parameter = Expression.Parameter(typeof(T), "obj");
+            var value = Expression.Parameter(typeof(TValue), "value");
+            var property = Expression.Property(parameter, propertyName);
+            var assign = Expression.Assign(property, value);
+            var lambda = Expression.Lambda<Action<T, TValue>>(assign, parameter, value);
+
+            return lambda.Compile();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        static void Main()
+        {
+            var people = new List<Person>
+        {
+            new Person { Name = "Alice", Age = 30 },
+            new Person { Name = "Bob", Age = 40 },
+            new Person { Name = "Charlie", Age = 35 },
+            new Person { Name = "Charlie2", Age = 37 }
+        };
+
+            // Biểu thức: p => p.Age > 34
+            ParameterExpression paramP = Expression.Parameter(typeof(Person), "p");
+            MemberExpression ageProperty = Expression.Property(paramP, "Age");
+            ConstantExpression ageValue = Expression.Constant(34);
+            BinaryExpression ageCondition = Expression.GreaterThan(ageProperty, ageValue);
+
+            // Biểu thức: p => p.Name.Contains("Charlie")
+            MemberExpression nameProperty = Expression.Property(paramP, "Name");
+            MethodCallExpression nameContainsMethod = Expression.Call(
+                nameProperty,
+                typeof(string).GetMethod("Contains", new Type[] { typeof(string) }),
+                Expression.Constant("Charlie")
+            );
+
+            // Kết hợp các điều kiện với AND: (p => p.Age > 34) AND (p => p.Name.Contains("Charlie"))
+            BinaryExpression combinedCondition = Expression.AndAlso(ageCondition, nameContainsMethod);
+
+            var lambda = Expression.Lambda<Func<Person, bool>>(combinedCondition, paramP).Compile();
+
+            var result = people.Where(lambda).ToList();
+
+            foreach (var person in result)
+            {
+                Console.WriteLine(person.Name); // Output: Charlie, Charlie2
+            }
+            Console.ReadKey();
+        }
+    }
+}
